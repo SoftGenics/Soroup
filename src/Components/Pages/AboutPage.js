594ad08ddfe220072143cf93bf7062c64c0e0649
Apoorv Kumar <96113133+SoftGenics@users.noreply.github.com>
2022-04-07 13:23:47 +0530

    Update AboutPage.js

--- src/Components/Pages/AboutPage.js
+++ src/Components/Pages/AboutPage.js
@@ -4,10 +4,10 @@ import Footer from '../Footer/Footer'
 const AboutPage = () => {
   return (
     <div>
-        <Container style={{marginTop:'10rem', fontSize:'40px', fontWeight:'700',justifyContent:'center', textAlign:'center', alignItems:'center'}}>
-            <h1 className='mt-3 mb-3'>ABOUT US</h1>
+        <Container style={{marginTop:'10rem', justifyContent:'center', textAlign:'center', alignItems:'center'}}>
+            <h1 className='mt-3 mb-3' style={{fontSize:'40px', fontWeight:'700'}}>ABOUT US</h1>
             <p className='card-text mt-5'>
-            <span style={{fontSize:'20px', fontWeight:'700', color:'red'}}>Formed </span>in the year 1990, Soroup Trading has continued to expand its capabilities to become one among the leading Importer & Dsitributor for FMCG & Non-Food Products in the Kingdom of Saudi Arabia.
+            <span style={{fontSize:'25px', fontWeight:'700', color:'red'}}>Formed </span>in the year 1990, Soroup Trading has continued to expand its capabilities to become one among the leading Importer & Dsitributor for FMCG & Non-Food Products in the Kingdom of Saudi Arabia.
             In line with its dynamic growth, Soroup trading has decided to diverisfy its portfolio and enter the Industrial Trading & Contracting sector. Using our portfolio of brands to differentiate our services and consumer products, we seek to develop the most innovative and profitable experiennce to out clients.
             </p>
       <p className='card-text'>We have established a strong foothold in the market by
