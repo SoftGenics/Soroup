d9485364aad38f4826d32f16b2c667c106537586
Apoorv Kumar <96113133+SoftGenics@users.noreply.github.com>
2022-04-08 16:37:03 +0530

    Update AboutPage.js

--- src/Components/Pages/AboutPage.js
+++ src/Components/Pages/AboutPage.js
@@ -3,26 +3,45 @@ import { Container } from 'react-bootstrap'
 import Footer from '../Footer/Footer'
 const AboutPage = () => {
   return (
-    <div>
-        <Container style={{marginTop:'10rem', justifyContent:'center', textAlign:'center', alignItems:'center'}}>
-            <h1 className='mt-3 mb-3' style={{fontSize:'40px', fontWeight:'700'}}>ABOUT US</h1>
-            <p className='card-text mt-5'>
-            <span style={{fontSize:'25px', fontWeight:'700', color:'red'}}>Formed </span>in the year 1990, Soroup Trading has continued to expand its capabilities to become one among the leading Importer & Dsitributor for FMCG & Non-Food Products in the Kingdom of Saudi Arabia.
-            In line with its dynamic growth, Soroup trading has decided to diverisfy its portfolio and enter the Industrial Trading & Contracting sector. Using our portfolio of brands to differentiate our services and consumer products, we seek to develop the most innovative and profitable experiennce to out clients.
-            </p>
-      <p className='card-text'>We have established a strong foothold in the market by
+    <>
+    <div style={{padding:'4rem'}}>
+        <Container style={{marginTop:'10rem',boxShadow:'0px 0px 8px black', padding:'3rem'}}>
+        <h1 className='mt-3 mb-3 text-center' ><span style={{color:'red'}} >ABOUT</span> US</h1>
+          <p>We have established a strong foothold in the market by
           introducing products of global standards from different
-          parts of the world.</p>
-        <p className='card-text'>Our well trained marketing personnel through its planned and
-            scheduled network system help streamline the supplies to every
-            nook and corner within the cities and their outskirts.</p>
-          <p className='card-text'>The mission of our customer care division is to visit every customer at
-                least once a week with a specialized team according to customer
-                demand or queries</p>
+          parts of the world.
+          
+          Our well trained marketing personnel through its planned and
+              scheduled network system help streamline the supplies to every
+              nook and corner within the cities and their outskirts.
+              The mission of our customer care division is to visit every customer at
+              least once a week with a specialized team according to customer
+              demand or queries</p>
+              <p>A major element to our competitive strength is our
+sales and distribution strategy anchored by our
+best-in-class sales team.</p>
+<p>Our well established distribution network will ensure that
+our products are well delivered and displayed on time at
+every outlets.</p>
+<p>ur facilities are capable of meeting the delivery requirements
+of diversified customer base ranging from Hyper Markets, Super
+Markets, A. B and C class markets. Wholesalers. Catering Companies.
+Restaurants, Hotels and Compound outlets etc.</p>
+<p>A major element to our competitive strength is our
+sales and distribution strategy anchored by our
+best-in-class sales team.</p>
+<p>Our well established distribution network will ensure that
+our products are well delivered and displayed on time at
+every outlets.
+Our facilities are capable of meeting the delivery requirements
+of diversified customer base ranging from Hyper Markets, Super
+Markets, A. B and C class markets. Wholesalers. Catering Companies.
+Restaurants, Hotels and Compound outlets etc.</p>
         </Container>
-        <Footer />  
+ 
     </div>
-
+    <Footer /> 
+    </>
   )
 }
 
